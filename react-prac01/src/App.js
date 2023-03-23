8c7b7aa18d39a191f9994223bb393cce126fde26
ssori <115159126+ssori0421@users.noreply.github.com>
2023-03-23 13:32:36 +0900

    react-prac01

---
+++ react-prac01/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Box from './component/Box';
+
+function App() {
+  return (
+    <div>
+      <Box name='리사' num='1' />
+      <Box name='제니' num='2' />
+      <Box name='지수' num='3' />
+    </div>
+  );
+}
+
+export default App;
