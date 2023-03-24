18402a13bfa34bde4e3949496e8fc4e4e838936d
ssori <115159126+ssori0421@users.noreply.github.com>
2023-03-24 20:38:38 +0900

    유저가 선택한 아이템 보여주기

--- react-project01/src/App.js
+++ react-project01/src/App.js
@@ -1,36 +1,39 @@
+import { useState } from 'react';
 import './App.css';
 import Box from './component/Box';
 
 const choice = {
   rock: {
     name: 'Rock',
-    img: 'https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FlIkI8%2Fbtr5E9wZ4Tu%2FO2LmLWZEvRh5kk79KILOFK%2Fimg.png',
+    img: 'https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2Fpjwah%2Fbtr5PaQSQhA%2FUhNJwVFscOYWh7y8vAsvd1%2Fimg.png',
   },
   scissors: {
     name: 'Scissors',
-    img: 'https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FyshIl%2Fbtr5HsJPmlG%2FmQfkz6rqgPHo9pp6T40Xo1%2Fimg.png',
+    img: 'https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FO1fxR%2Fbtr5N4KCpZ1%2FnVgmP96qZKDKgnHG9jJGu0%2Fimg.png',
   },
   papper: {
     name: 'Papper',
-    img: 'https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FkzKeo%2Fbtr5HsC3Nec%2FNYmAjmqIG66i44KGiqIMgK%2Fimg.png',
+    img: 'https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FbmRJgA%2Fbtr5N7NTKuK%2FZF2wfKbjyBuEzyrm1QqRuK%2Fimg.png',
   },
 };
 function App() {
+  const [userSelect, setUserSelect] = useState(null);
   const play = (userChoice) => {
-    console.log('선택', userChoice);
+    // console.log('선택', userChoice);
+    setUserSelect(choice[userChoice]);
   };
 
   return (
     <div>
       <div className='Box-Wrap'>
-        <Box title='You' />
-        <Box title='Computer' />
+        <Box title='You' item={userSelect} />
+        {/*   */}
       </div>
       <div className='Button-wrap'>
         {/* play함수의 매개변수: scissors, rock, paper */}
         <button onClick={() => play('scissors')}>가위</button>
         <button onClick={() => play('rock')}>바위</button>
-        <button onClick={() => play('paper')}>보</button>
+        <button onClick={() => play('papper')}>보</button>
       </div>
     </div>
   );
--- react-project01/src/component/Box.js
+++ react-project01/src/component/Box.js
@@ -1,13 +1,11 @@
 import React from 'react';
 
 const Box = (props) => {
+  console.log('props', props); // props 객체의 item 속성 확인
   return (
     <div className='box'>
       <h1>{props.title}</h1>
-      <img
-        className='item-img'
-        src='https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2Fbd66bF%2Fbtr5D2kASZx%2FUaY4Sb3vnSYBbVCrLkay4K%2Fimg.png'
-      />
+      <img className='item-img' src={props.item && props.item.img} />
       <h2>win</h2>
     </div>
   );
