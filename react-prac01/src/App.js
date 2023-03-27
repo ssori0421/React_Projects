c15081633c5ddbaa1aad6924c65bcd068decfbd9
ssori <115159126+ssori0421@users.noreply.github.com>
2023-03-27 15:30:47 +0900

    git commit -m "className 추가"

--- react-prac01/src/App.js
+++ react-prac01/src/App.js
@@ -3,7 +3,7 @@ import Box from './component/Box';
 
 function App() {
   return (
-    <div>
+    <div className='box'>
       <Box name='리사' num='1' />
       <Box name='제니' num='2' />
       <Box name='지수' num='3' />
