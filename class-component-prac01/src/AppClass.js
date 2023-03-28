da85908bdd4d21305db9800e28e4946bda5cca68
ssori <115159126+ssori0421@users.noreply.github.com>
2023-03-28 21:15:19 +0900

    class 컴포넌트lifecycle 확인

--- class-component-prac01/src/AppClass.js
+++ class-component-prac01/src/AppClass.js
@@ -9,20 +9,31 @@ export default class AppClass extends Component {
       num: 1,
       value: 0,
     };
+    console.log('constructor'); // class컴포넌트 Mounting 확인
   }
   increase = () => {
     this.setState({
       counter2: this.state.counter2 + 1,
       value: this.state.value + 1,
     });
+    console.log('increase', this.state); // useStatete는 비동기적으로 실행되므로 바로 반영 안됨!
   };
 
+  componentDidMount() {
+    console.log('componentDidMount'); // class컴포넌트 Mounting 확인
+  }
+
+  componentDidUpdate() {
+    console.log('componentDidUpdate', this.state); // class컴포넌트 Updating 확인
+  }
+
   render() {
+    console.log('render'); // class컴포넌트 Mounting, Updating 확인
     return (
       <div>
         <div>state:{this.state.counter2}</div>
         <button onClick={this.increase}>클릭!</button>
-        <BoxClass number={this.state.value} />
+        {this.state.counter2 < 3 && <BoxClass number={this.state.value} />}
       </div>
     );
   }
--- class-component-prac01/src/component/BoxClass.js
+++ class-component-prac01/src/component/BoxClass.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 
 export default class BoxClass extends Component {
+  componentWillUnmount() {
+    console.log('componentWillUnmount'); // class컴포넌트 Unmounting 확인
+  }
   render() {
     return <div>Box{this.props.number}</div>;
   }
