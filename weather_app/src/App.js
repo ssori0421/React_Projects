f808828e733e3e667600488a2a911fff49843ce5
ssori <115159126+ssori0421@users.noreply.github.com>
2023-03-29 10:25:46 +0900

    현재 위치 가져오기

--- weather_app/src/App.js
+++ weather_app/src/App.js
@@ -1,7 +1,25 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
+// 1. 앱이 실행되자 마자 현재위치 기반의 날씨가 보인다
+// 2. 날씨 상태 정보에는 도시, 섭씨, 화씨
+// 3. 5개의 버튼(1개는 현재 위치, 4개는 다른 도시)
+// 4. 도시 버튼을 클릭하면 도시별 날씨가 나옴
+// 5. 현재위치 버튼을 누르면 다시 현재위치 기반의 날씨가 나옴
+// 6. 로딩 스피너
+
 function App() {
+  const getCurrentLocation = () => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      let lat = position.coords.latitude;
+      let lon = position.coords.longitude;
+      console.log('현재위치', lat, lon);
+    });
+  };
+  useEffect(() => {
+    getCurrentLocation();
+  }, []);
+
   return <div>hi</div>;
 }
 
