3c33d9625b650c6d4754e4a1e3b3d9aa83b9a0d7
ssori <115159126+ssori0421@users.noreply.github.com>
2023-03-30 14:00:16 +0900

    react-router설치 및 route설정

---
+++ router_example/src/page/Aboutpage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Aboutpage = () => {
+  return (
+    <div>
+      <h1>Aboutpage입니다.</h1>
+    </div>
+  );
+};
+
+export default Aboutpage;
---
+++ router_example/src/page/Homepage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Homepage = () => {
+  return (
+    <div>
+      <h1>Homepage입니다.</h1>
+    </div>
+  );
+};
+
+export default Homepage;
