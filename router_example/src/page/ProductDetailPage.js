3bbb95a6dc75d3439de45b5e7fa02481117a63d8
ssori <115159126+ssori0421@users.noreply.github.com>
2023-03-30 19:26:47 +0900

    useParams를 사용해서 url에서 parameter 읽어오기

--- router_example/src/page/ProductDetailPage.js
+++ router_example/src/page/ProductDetailPage.js
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from 'react';
+import { useParams } from 'react-router-dom';
 
 const ProductDetailPage = () => {
+  const { id } = useParams(); // Destructuring을 사용해 변수에 필요한 값만 전달
+  // console.log('params', params); // params는 object
   return (
     <div>
-      <h1>Show Product Detail</h1>
+      <h1>Show Product Detail{id}</h1>
     </div>
-  )
-}
+  );
+};
 
-export default ProductDetailPage
+export default ProductDetailPage;
