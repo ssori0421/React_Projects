0fdc354638b41682a047c804696bffb1aef53013
ssori <115159126+ssori0421@users.noreply.github.com>
2023-03-30 19:45:31 +0900

    useSearchParams를 사용해서 url에서 query 읽어오기

--- router_example/src/page/Homepage.js
+++ router_example/src/page/Homepage.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 const HomePage = () => {
+  const navigate = useNavigate();
+  const goToProductPage = () => {
+    navigate('/products?q=pants');
+  };
+  
   return (
     <div>
       <h1>HomePage입니다.</h1>
       <Link to='/about'>Go to AboutPage</Link>
+      <button onClick={goToProductPage}>go to ProductPage </button>
     </div>
   );
 };
--- router_example/src/page/ProductPage.js
+++ router_example/src/page/ProductPage.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import { useSearchParams } from 'react-router-dom';
 
 const ProductPage = () => {
+  let [query, setQuery] = useSearchParams();
+  console.log('q', query.get('q')); // q 쿼리에 있는 값을 가져와줘
   return (
     <div>
       <h1>Show All Products</h1>
