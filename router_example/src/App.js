2934f9fcad551e29c68e5065ff6298c8873616e4
ssori <115159126+ssori0421@users.noreply.github.com>
2023-03-30 20:53:00 +0900

    Navigate를 사용해서 페이지를 보호하기

--- router_example/src/App.js
+++ router_example/src/App.js
@@ -1,19 +1,28 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 import HomePage from './page/HomePage';
 import AboutPage from './page/AboutPage';
 import ProductPage from './page/ProductPage';
 import ProductDetailPage from './page/ProductDetailPage';
-import { Routes, Route } from 'react-router-dom';
+import SignInPage from './page/SignInPage';
+import UserPage from './page/UserPage';
+import { Routes, Route, Navigate } from 'react-router-dom';
 
 function App() {
+  // authenticate 값이 true이면 로그인 한 거라고 가정!
+  const [authenticate, setAuthenticate] = useState(false);
+  const PrivateRoute = () => {
+    return authenticate === true ? <UserPage /> : <Navigate to='/signin' />;
+  };
   return (
     <div>
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='/about' element={<AboutPage />} />
-        <Route path='/Products' element={<ProductPage />} />
-        <Route path='/Products/:id' element={<ProductDetailPage />} />
+        <Route path='/products' element={<ProductPage />} />
+        <Route path='/products/:id' element={<ProductDetailPage />} />
+        <Route path='/signin' element={<SignInPage />} />
+        <Route path='/user' element={<PrivateRoute />} />
       </Routes>
     </div>
   );
