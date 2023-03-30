5e51cacfe798cf9051af8a1216548fb5bd8bd8c6
ssori <115159126+ssori0421@users.noreply.github.com>
2023-03-30 11:42:32 +0900

    로딩 스피너 구현

--- weather_app/src/App.js
+++ weather_app/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import WeatherBox from './component/WeatherBox';
 import ButtonBox from './component/ButtonBox';
+import ClipLoader from 'react-spinners/ClipLoader';
 
 // 1. 앱이 실행되자 마자 현재위치 기반의 날씨가 보인다
 // 2. 날씨 상태 정보에는 도시, 섭씨, 화씨
@@ -14,6 +15,7 @@ import ButtonBox from './component/ButtonBox';
 function App() {
   const [weather, setWeather] = useState(null); // weather 데이터를 넣을 state 만들기
   const [city, setCity] = useState('');
+  const [loading, setLoading] = useState(false); // loading 값이 true 일 때에만 로딩스피너가 보이게 됨
   const cities = ['paris', 'new york', 'tokyo', 'seoul'];
 
   const getCurrentLocation = () => {
@@ -26,18 +28,22 @@ function App() {
 
   const getWeatherByCurrentLocation = async (lat, lon) => {
     let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=e919bb3b5b58a2b3ce50ef98fb148e5a&units=metric  `;
+    setLoading(true);
     let response = await fetch(url);
     let data = await response.json();
     // console.log('data', data);
     setWeather(data); // weather state에 데이터를 넣어줘
+    setLoading(false);
   }; // getWeatherByCurrentLocation 함수는 lat, lon을 매개변수로 넘겨받아 url을 생성하는 작업을 함
 
   const getWeatherByCity = async () => {
     let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=e919bb3b5b58a2b3ce50ef98fb148e5a&units=metric`;
+    setLoading(true);
     let response = await fetch(url);
     let data = await response.json();
     // console.log('data', data);
     setWeather(data); // weather state에 데이터를 넣어줘
+    setLoading(false);
   };
 
   useEffect(() => {
@@ -50,10 +56,16 @@ function App() {
 
   return (
     <div>
-      <div className='weatherContainer'>
-        <WeatherBox weather={weather} />
-        <ButtonBox cities={cities} setCity={setCity} />
-      </div>
+      {loading ? (
+        <div className='weatherContainer'>
+          <ClipLoader color='#000000' loading={loading} size={150} />
+        </div>
+      ) : (
+        <div className='weatherContainer'>
+          <WeatherBox weather={weather} />
+          <ButtonBox cities={cities} setCity={setCity} />
+        </div>
+      )}
     </div>
   );
 }
