c37a614b87d05244bfb49455e73ef3854f28d7f3
ssori <115159126+ssori0421@users.noreply.github.com>
2023-04-04 22:07:16 +0900

    api 호출

--- shopping_mall_website/src/page/ProductListPage.js
+++ shopping_mall_website/src/page/ProductListPage.js
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 
 const ProductListPage = () => {
-  return (
-    <div>
-    </div>
-  )
-}
+  const [productList, setProductList] = useState([]);
+  const getProducts = async () => {
+    const url = 'http://localhost:5000/products';
+    const response = await fetch(url); // url을 fetch해줘
+    const data = await response.json(); // response에서 json 추출
+    setProductList(data);
+  };
+  useEffect(() => {
+    getProducts();
+  }, []);
 
-export default ProductListPage
+  return <div></div>;
+};
+
+export default ProductListPage;
