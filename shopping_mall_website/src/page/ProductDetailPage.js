4a2dde0b837a27fb5dd98de11a036b8bdfc50f51
ssori <115159126+ssori0421@users.noreply.github.com>
2023-04-05 21:46:50 +0900

    상품 디테일 페이지에 데이터 가져오기

--- shopping_mall_website/src/page/ProductDetailPage.js
+++ shopping_mall_website/src/page/ProductDetailPage.js
@@ -1,6 +1,18 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
 const ProductDetailPage = () => {
+  let { id } = useParams();
+  const [product, setProduct] = useState(null);
+  const getProductDetail = async () => {
+    const url = `http://localhost:5000/products/${id}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    setProduct(data);
+  };
+  useEffect(() => {
+    getProductDetail();
+  }, []);
   return <div></div>;
 };
 
